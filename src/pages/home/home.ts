aac518d15edd28c350f3b8eb063bbec1426ae1d4
moycs777 <moycs777@gmail.com>
2021-12-30 23:18:08 -0300

    init state

---
+++ src/pages/home/home.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+import { FormGroup, FormControl,FormArray, FormBuilder } from '@angular/forms'
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  myObj = {
+    'bnatch1': {
+      name: 'bnatch1',
+      pages : []
+    },
+    'bnatch2': {
+      name: 'bnatch2',
+      pages : []
+    },
+
+  };
+
+  skillsForm: FormGroup;
+  batchNamesForm: FormGroup;
+
+  constructor(
+    private fb:FormBuilder
+  ) {
+    this.prepareForm();
+  }
+
+  prepareForm() {
+    this.skillsForm = this.fb.group({
+      name: '',
+      skills: this.fb.array([]) ,
+    });
+
+    setTimeout(() => {
+      this.Data()
+    }, 1200);
+  }
+
+  Data() {
+     Object.keys(this.myObj).forEach((batchName, index) => {
+      console.log('this.myObj[batchName]', this.myObj[batchName].name);
+      this.skills.push(
+        this.fb.group({
+          skill: this.myObj[batchName].name,
+        }
+      ));
+    });
+  }
+
+  get skills() : FormArray {
+    return this.skillsForm.get("skills") as FormArray
+  }
+
+  onSubmit() {
+    console.log(this.skillsForm.value);
+  }
+}
